946cd3c1a7016225768d58327fa9d9a8c227f0e0
Diether Dayondon <diether.dayondon@g.msuiit.edu.ph>
2018-10-04 21:15:30 +0800

    Landing Page: Initial Landing Page Layout

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, redirect, request, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('home.html', title='Home Page')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Hap')
+
+if __name__ == '__main__':
+    app.run(debug=True)
