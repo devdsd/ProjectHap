ed30c48cd647928af085cdbdea9a03e7d6298c07
Francis Mascariñas <fmscrns@gmail.com>
2018-11-06 00:12:51 +0800

    (1)New UI (2)Home can now populate feed with event posts

--- hap/forms.py
+++ hap/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NumberRange
 from wtforms.fields.html5 import DateField
+from wtforms_components import TimeField
 from hap.models import Users
 
 class SignupForm(FlaskForm):
@@ -12,28 +12,28 @@ class SignupForm(FlaskForm):
     lastName = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=50)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=50)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('SIGN UP')
 
     def validate_username(self, username):
 		user = Users.query.filter_by(username=username.data).first()
 		if user:
-			raise ValidationError('That username already exists. Please choose different one!')
+			raise ValidationError("Username already taken.")
 
     def validate_email(self, email):
 		user = Users.query.filter_by(email=email.data).first()
 		if user:
-			raise ValidationError('That email is already exists. Please choose different one!')
+			raise ValidationError("Email already used.")
 
 class LoginForm(FlaskForm):
   email = StringField('Email', validators=[DataRequired(), Email()])
   # username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-  password = PasswordField('Password', validators=[DataRequired()])
-  remember = BooleanField('Remember Me')
+  password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=50)])
+  remember = BooleanField('Remember Password')
   
-  submit = SubmitField('Login')
+  submit = SubmitField('LOG IN')
 
 	
 
@@ -41,9 +41,12 @@ class CreateEventForm(FlaskForm):
     eventName = StringField('Event Name', validators=[DataRequired(), Length(min=2, max =50)])
     eventDescription = TextAreaField('Event Description', validators=[DataRequired()])
     eventDate = DateField('Event Date', format='%Y-%m-%d')
-    fee = IntegerField('Fee')
-    location = StringField('Location', validators=[DataRequired(), Length(min=2, max =50)])
-    submit = SubmitField('Submit')
+    startTime = TimeField("Event Start Time", validators=[DataRequired()])
+    endTime = TimeField("Event End Time", validators=[DataRequired()])
+    imageFile = FileField("Profile Picture", validators=[FileAllowed(["jpg", "png"])])
+    fee = IntegerField("Fee", validators=[DataRequired(), NumberRange(max=1000000)])
+    location = StringField('Location', validators=[DataRequired(), Length(min=2, max =75)])
+    submit = SubmitField('CREATE EVENT')
 
 class UpdateAccountForm(FlaskForm):
       firstName = StringField('First Name', validators=[DataRequired(), Length(min=2, max=50)])
@@ -51,7 +54,7 @@ class UpdateAccountForm(FlaskForm):
       username = StringField('Username', validators=[DataRequired(),Length(min=2, max=20)])
       email = StringField('Email', validators=[DataRequired(), Email()])
       picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-      submit = SubmitField('Update')
+      submit = SubmitField('UPDATE')
 
       # def validate_firstName(self, firstName):
       #   if firstName.data != current_user.firstName:
@@ -70,15 +73,10 @@ class UpdateAccountForm(FlaskForm):
         if username.data != current_user.username:
           user = Users.query.filter_by(username=username.data).first()
           if user:
-            raise ValidationError('That username is taken. Please choose another name')
+            raise ValidationError("Username already taken.")
       
       def validate_email(self, email):
         if email.data != current_user.email:
           user = Users.query.filter_by(email=email.data).first()
           if user:
-              raise ValidationError('That email is Taken.')
-      eventDescription = TextAreaField('Event Description', validators=[DataRequired()])
-      eventDate = DateField('Event Date', format='%Y-%m-%d')
-      fee = IntegerField('Fee')
-      location = StringField('Location', validators=[DataRequired(), Length(min=2, max =50)])
-      submit = SubmitField('Submit')
+              raise ValidationError("Email already used.")
--- hap/routes.py
+++ hap/routes.py
@@ -7,36 +7,64 @@ from hap.forms import SignupForm, LoginForm, CreateEventForm, UpdateAccountForm
 from hap.models import *
 from flask_login import login_user, current_user, logout_user, login_required
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', title='Home Page')
+def save_picture(form_picture):
+    random_hex = binascii.b2a_hex(os.urandom(15))
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/images', picture_fn)
+    
+    output_size=(400,400)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
 
+@app.route('/', methods=["GET","POST"])
+@app.route('/home', methods=["GET","POST"])
+def home():
+    if current_user.is_authenticated:
+        formTwo = CreateEventForm()
+        events = Events.query.all()
+        if formTwo.validate_on_submit():
+            picture_file = ""
+            if formTwo.imageFile.data:
+                picture_file = save_picture(formTwo.imageFile.data)
+
+            event = Events(image_file=picture_file, eventName=formTwo.eventName.data, eventDate=formTwo.eventDate.data, eventStartTime=formTwo.startTime.data, eventEndTime=formTwo.endTime.data, eventDescription=formTwo.eventDescription.data, fee=formTwo.fee.data, location=formTwo.location.data, host=current_user)
+            
+            db.session.add(event)
+            db.session.commit()
+
+            flash("Your event has been created.", "success")
+            return redirect(url_for("home"))
+        
+        elif formTwo.eventName.data:
+            flash("Create unsuccessful. Please try again.", "danger")
+
+        return render_template("home.html", title="Home", formTwo=formTwo, homeNavbarLogoBorderBottom="#FFC000", profileNavbarLogoBorderBottom="white", events=events)
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
+        else:
+            flash("Log in unsuccessful.", "danger")
+    elif form.email.data or form.password.data:
+        return render_template("home.html", form=form, title="Welcome to Hap!", dropdownAppearance="show", ariaExpansionBool="true")
+        
+    return render_template("home.html", form=form, title="Welcome to Hap!", dropdownAppearance="", ariaExpansionBool="false")
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Hap')
 
-
-@app.route('/createevent', methods=['GET', 'POST'])
-@login_required
-def createevent():
-    form = CreateEventForm()
-    if form.validate_on_submit():
-
-        event = Events(eventName=form.eventName.data, eventDate=form.eventDate.data, eventDescription=form.eventDescription.data, location=form.location.data,  fee=form.fee.data, user_id=current_user.id)
-        db.session.add(event)
-        db.session.commit()
-
-        flash('Event Created successfully', 'success')
-        
-        return redirect(url_for('home'))
-    return render_template('createevent.html', title='Create Event', form=form)
-
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     
@@ -48,14 +76,13 @@ def signup():
         db.session.add(user)
         db.session.commit()
 
-        flash('Account successfully created! You can now log in', 'success')
+        flash("Account created successfully.", "success")
         return redirect(url_for('login'))
-    return render_template('signup.html', title='Sign Up', form=form)
 
+    return render_template('signup.html', title='Sign Up', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     
@@ -66,10 +93,11 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('account'))
+            return redirect(next_page) if next_page else redirect(url_for("home"))
             # return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful! Please check username/email and password', 'danger')
+            flash("Log in unsuccessful.", "danger")
+
     return render_template('login.html', title='Log In', form=form)
 
 @app.route('/logout')
@@ -77,40 +105,43 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-def save_picture(form_picture):
-    random_hex = binascii.b2a_hex(os.urandom(15))
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path,'static/profile_pics', picture_fn)
-    
-    output_size=(125,125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-    
-    return picture_fn
-
-
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+
+    profilePic = url_for("static", filename="images/" + current_user.image_file)
+    return render_template("account.html", title="Account", homeNavbarLogoBorderBottom="white", profileNavbarLogoBorderBottom="#FFC000", profilePic=profilePic)
+
+@app.route("/settings", methods=["GET", "POST"])
+@login_required
+def settings():
     form = UpdateAccountForm()
+
     if form.validate_on_submit():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
-        current_user.username = form.username.data
-        current_user.email = form.email.data
         current_user.firstName = form.firstName.data
         current_user.lastName = form.lastName.data
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+            
         db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('account'))
 
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-        form.firstName.data = current_user.firstName
-        form.lastName.data = current_user.lastName
-    image_file = url_for('static', filename='images/' + current_user.image_file)
-    return render_template('account.html', title = 'Account', image_file=image_file, form=form)
+        flash("You profile has been updated.", "success")
+        
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.firstName.data = current_user.firstName;
+        form.lastName.data = current_user.lastName;
+        form.username.data = current_user.username;
+        form.email.data = current_user.email;
+
+    return render_template("settings.html", title="Settings", form=form, homeNavbarLogoBorderBottom="white", profileNavbarLogoBorderBottom="white")
+
+@app.route("/event/<int:event_id>")
+@login_required
+def event(event_id):
+    event = Events.query.get_or_404(event_id)
+    
+    return render_template("event.html", title=event.eventName, event=event, homeNavbarLogoBorderBottom="white", profileNavbarLogoBorderBottom="white")
