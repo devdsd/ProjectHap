c6a1a71a5f54041e9f9886fcb5f18c32e8dc3b70
Diether Dayondon <diether.dayondon@g.msuiit.edu.ph>
2018-11-26 13:36:30 +0800

    For pulling

--- hap/routes.py
+++ hap/routes.py
@@ -25,8 +25,9 @@ def save_picture(form_picture, size):
 def home():
     if current_user.is_authenticated:
         formTwo = CreateEventForm()
-        events = Events.query.order_by(Events.dateCreated.desc())
-        # events = Events.query.filter(Categories.eventhascategory.any(id=event.id)).order_by(Events.dateCreated.desc()).all()
+        # events = Events.query.order_by(Events.dateCreated.desc())
+        userinterest = userhasinterest_rel_table.query.filter().first()
+        events = Events.query.filter(Categories.eventhascategory.any(id=category.id)).order_by(Events.dateCreated.desc()).all()
         # print "Ambot nimo !"
 
         if formTwo.validate_on_submit():
